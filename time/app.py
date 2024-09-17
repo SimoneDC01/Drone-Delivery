dbe7bd85f9ee9609e6b0b8df01b709e0322c3c4b
Aleee01 <alessandragio2001@gmail.com>
2024-09-17 17:42:43 +0200

    time-scheduler interaction completed

--- time/app.py
+++ time/app.py
@@ -14,7 +14,7 @@ def get_db_connection():
 @app.route('/advance', methods=['POST'])
 def getDeliveryInfo():
     minutes = request.get_json()['minutes']
-    #upgrade date and time values in database
+    #select date and time values in database
     conn = get_db_connection()
     query = "SELECT date, time FROM time LIMIT 1"
     row = conn.execute(query).fetchone()
@@ -29,19 +29,20 @@ def getDeliveryInfo():
         hour = int(time_parts[0])
         min = int(time_parts[1])
 
+        date = {'day' : day, 'month': month, 'year' : year}
+        time = {'hh' : hour, 'mm' : min }
+
         min += int(minutes)
-        # Gestisci l'overflow di minuti
+
         if min > 59:
             min -= 60
             hour += 1
 
-        # Gestisci l'overflow di ore
         if hour >= 24:
             hour -= 24
             day += 1
-            # Gestisci l'overflow di giorni
+
             days_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-            # Controlla se l'anno è bisestile per febbraio
             if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
                 days_in_month[1] = 29
             else:
@@ -50,32 +51,28 @@ def getDeliveryInfo():
             if day > days_in_month[month - 1]:
                 day = 1
                 month += 1
-
-                # Gestisci l'overflow di mesi
                 if month > 12:
                     month = 1
                     year += 1
 
-    # Ricostruisci la stringa della nuova data e ora
     new_date = f"{day:02d}/{month:02d}/{year:04d}"
     new_time = f"{hour:02d}:{min:02d}"
 
-    # Esegui l'UPDATE del database con la nuova data e ora
+    url = 'http://scheduler:8080/advance'
+    data = {'minutes': minutes, 'date': date, 'time' : time}
+    response = requests.post(url, json=data)
+
+    #update of date and time
     update_query = "UPDATE time SET date = ?, time = ?"
     conn.execute(update_query, (new_date, new_time))
-    conn.commit()  # Salva le modifiche nel database
+    conn.commit()  
 
-    # Chiudi la connessione
     conn.close()
-    url = 'http://scheduler:8080/advance'
-    data = {'minutes': minutes}
-    response = requests.post(url, json=data)
+
     url = 'http://user-manager:8080/date_and_time_new'
     data = {'date_and_time': new_date+" "+new_time}
     requests.post(url, json=data)
-    #take date and time from database
     return jsonify({'date': new_date, 'time': new_time})
-    #return jsonify("data + time "+response.text + '[TIME]')
 
 @app.route('/date_and_time_request', methods=['POST'])
 def date_and_time_request():
@@ -89,12 +86,8 @@ def date_and_time_request():
     else:
         date_time_data = {'error': 'No data found'}
 
-    # Restituisci i dati in formato JSON
     return jsonify(date_time_data)
-    
-#def date_and_time_request():
-    #take date and time from database
-    #return jsonify("date + time" + '[TIME]')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
