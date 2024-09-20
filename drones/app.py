1c9d235765de5947fadff9597114087b2ce4619d
andreaciprotti <131988167+andreaciprotti@users.noreply.github.com>
2024-09-20 11:37:33 +0200

    parameters adjustement

--- drones/app.py
+++ drones/app.py
@@ -2,27 +2,23 @@ from flask import Flask, request, jsonify
 import requests
 import random
 
-# Drones status
-CHARGING = 0
-DELIVERING = 1
-GOING_BACK = 2
 
-MAX_BATTERY = 120
+MAX_BATTERY1 = 240
+MAX_BATTERY2 = 120
 CHARGE_VELOCITY = 3
 class Drone:
     def __init__(self):
-        self.battery1 = MAX_BATTERY
-        self.battery2 = MAX_BATTERY
-        self.status = CHARGING
+        self.battery1 = MAX_BATTERY1
+        self.battery2 = MAX_BATTERY2
         self.is_strong_wind = False
         self.states_history = [0, 0]
 
     def __str__(self):
         return ("{" + f"battery1 : {self.battery1}, battery2 : {self.battery2}, "
-                f"status : {self.status}, is_strong_wind : {self.is_strong_wind}, states_history : {self.states_history}" + "}")
+                f"is_strong_wind : {self.is_strong_wind}, states_history : {self.states_history}" + "}")
     def __repr__(self):
         return ("{" + f"battery1 : {self.battery1}, battery2 : {self.battery2}, "
-                f"status : {self.status}, is_strong_wind : {self.is_strong_wind}, states_history : {self.states_history}" + "}")
+                f"is_strong_wind : {self.is_strong_wind}, states_history : {self.states_history}" + "}")
 
 transition_states = {
     (0, 0): [0.95, 0.05],   # From (No Wind, No Wind) to No Wind o Strong Wind
@@ -48,13 +44,13 @@ def extract_time(time):
     return hours, minutes
 
 # given the package information and the time, return if the drone is delivering or going back
-def get_deliver_status(package, time) :
+def going_back(package, time) :
     deliver_duration = extract_time(package['time']['end'])[1] + (extract_time(package['time']['end'])[0] - extract_time(package['time']['start'])[0]) * 60 - extract_time(package['time']['start'])[1]
     half_delivery_time = extract_time(package['time']['start'])[0] + (deliver_duration/2 + extract_time(package['time']['start'])[1]) // 60, (extract_time(package['time']['start'])[1] + (deliver_duration/2)) % 60
-    if(time <= half_delivery_time) :
-        return DELIVERING
+    if(time == half_delivery_time) :
+        return True
     else :
-        return GOING_BACK
+        return False
     
     
 app = Flask(__name__)
@@ -83,19 +79,18 @@ def advance():
             if(time<extract_time(package['time']['end'])):
                 # the drone is charging
                 if(package['index'] == 'recharge') :
-                    my_drones[drone].status = CHARGING
                     my_drones[drone].is_strong_wind = False
                     my_drones[drone].states_history.append(0)
                     
-                    if my_drones[drone].battery1 < MAX_BATTERY :
+                    if my_drones[drone].battery1 < MAX_BATTERY1 :
                         my_drones[drone].battery1 += CHARGE_VELOCITY
-                        if my_drones[drone].battery1 > MAX_BATTERY :
-                            my_drones[drone].battery1 =  MAX_BATTERY
+                        if my_drones[drone].battery1 > MAX_BATTERY1 :
+                            my_drones[drone].battery1 =  MAX_BATTERY1
                     
-                    if my_drones[drone].battery2 < MAX_BATTERY :
+                    if my_drones[drone].battery2 < MAX_BATTERY2 :
                         my_drones[drone].battery2 += CHARGE_VELOCITY
-                        if my_drones[drone].battery2 > MAX_BATTERY :
-                            my_drones[drone].battery2 =  MAX_BATTERY
+                        if my_drones[drone].battery2 > MAX_BATTERY2 :
+                            my_drones[drone].battery2 =  MAX_BATTERY2
                 # simulate the wind and update involved values
                 else :
                     wind = get_wind(my_drones[drone].states_history)
@@ -106,11 +101,16 @@ def advance():
                     else:
                         my_drones[drone].is_strong_wind = False
 
-                    my_drones[drone].status = get_deliver_status(package, time)  
+                    if going_back(package, time) :
+                        url = 'http://scheduler:8080/delivered'
+                        data = {'order_package': package['index']}
+                        requests.post(url, json=data)
                     
                 break
+        
+        #finish
 
-    return str(my_drones)                # { 'drone1' : {battery1 : value, battery2 : value, status : value, is_strong_wind : value }, 'drone2' : {battery1 : value, battery2 : value, status : value, is_strong_wind : value }, ... } 
+    return str(my_drones)                # { 'drone1' : {battery1 : value, battery2 : value, is_strong_wind : value }, 'drone2' : {battery1 : value, battery2 : value, is_strong_wind : value }, ... } 
 
 if __name__ == '__main__':                      
     app.run(debug=True)
