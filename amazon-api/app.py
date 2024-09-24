56bb5cb6ad10ecb205b914c39664fec7fbf8dbd0
Simone Di Cocco <simonedc01@gmail.com>
2024-09-24 16:11:17 +0200

    amazon-api implementation

--- amazon-api/app.py
+++ amazon-api/app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -5,10 +6,39 @@ app = Flask(__name__)
 @app.route('/getProductsInfo', methods=['POST'])
 def getProductsInfo():
     asins = request.get_json()['asins']
-    if isinstance(asins, list):
-        return [{'description':'prod1', 'weight': 30, 'dimension': '20x30x15'}, {'description':'prod2', 'weight': 40, 'dimension': '10x30x15'}]
-    else:
-        return [{'description':'prod1', 'weight': 30, 'dimension': '20x30x15'}, {'description':'prod2', 'weight': 40, 'dimension': '10x30x15'}]
+
+    url = "https://real-time-amazon-data.p.rapidapi.com/product-details"
+    print('asins:', asins)
+    querystring = {"asin": asins, "country":"IT"}
+
+    headers = {
+        "x-rapidapi-key": "139df1d6bamshac56b455af54eddp1bf893jsne5df5fd73e2a",
+        "x-rapidapi-host": "real-time-amazon-data.p.rapidapi.com"
+    }
+
+    api_response = requests.get(url, headers=headers, params=querystring)
+    
+    api_response = api_response.json()
+    
+    response = []
+    for el in api_response['data'] :
+        new_product = {}
+        new_product['description'] = el['product_title']
+        if 'Dimensioni prodotto' in list(el['product_information'].keys()):
+            dimension, weight = el['product_information']['Dimensioni prodotto'].split(';')
+            dimension = dimension.replace(' ','')
+            dimension = dimension.replace('cm','')
+            weight = weight.replace('kg','')
+            weight = weight.replace(' ','')
+            new_product['weight'] = weight
+            new_product['dimension'] = dimension
+        else :
+            new_product['weight'] = None
+            new_product['dimension'] = None
+        
+        response.append(new_product)
+    
+    return response
     
 if __name__ == '__main__':
     app.run(debug=True)
